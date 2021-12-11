ad06a15cf32f21dd7058c2fb882db840c6149774
Ahmad Pourihosseini <a.pourihosseini@gmail.com>
2021-12-11 15:00:46 -0500

    Write basic methods for RecordStore

--- data.py
+++ data.py
@@ -1,28 +1,47 @@
 from typing import *
-
+import pickle
 
 class Record:
-    def __init__(self):
-        self.id: int = None
-        self.cls: str = None
-        self.title: str = None
-        self.synopsis: str = None
+    def __init__(self, cls, title, synopsis, id):
+        self.cls: str = cls
+        self.title: str = title
+        self.synopsis: str = synopsis
+        self.id: int = id
             
         self.title_embeddings_bert: List[Any] = None
         self.synopsis_embeddings_bert: List[Any] = None
 
         self.title_embeddings_w2v: List[Any] = None
         self.synopsis_embeddings_w2v: List[Any] = None
+
+    def __str__(self) -> str:
+        return "class: {}, title: {}, id:{}, synopsis:{}".format(self.cls, self.title, self.id, self.synopsis)
         
 class RecordStore:
-    def __init__(self) -> None:
-        pass
+    train_records = []
+    test_records = []
 
-    def load_bert(self):
+    @staticmethod
+    def load():
+        with open('trainTest.pk', 'rb') as f:
+            classes = pickle.load(f)
+            train = pickle.load(f)
+            test = pickle.load(f)
+
+        RecordStore.train_records = [Record(cls, title, synopsis, id) for (cls, title, synopsis, id) in train]
+        RecordStore.test_records = [Record(cls, title, synopsis, id) for (cls, title, synopsis, id) in test]
+    
+    @staticmethod
+    def load_bert():
         pass # TODO
 
-    def load_w2v(self):
-        pass # TODO
+    @staticmethod
+    def load_w2v():
+        with open('w2v_train.pk', 'rb') as f:
+            [title_embeddings, synopsis_embeddings] = pickle.load(f)
 
-    def get_record(i: int) -> Record:
-        pass 
+        for i, record in enumerate(RecordStore.train_records):
+            record.title_embeddings_w2v = title_embeddings[i]
+            record.synopsis_embeddings_w2v = synopsis_embeddings[i]
+    
+
