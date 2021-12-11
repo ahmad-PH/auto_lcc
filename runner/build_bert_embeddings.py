8c8ef1492bf7dfe0280e155563237e2659e54f43
Teerapat12 <teerapat.time12@gmail.com>
2021-12-11 16:45:50 -0500

    code to build bert with work around done

--- runner/build_bert_embeddings.py
+++ runner/build_bert_embeddings.py
@@ -1,4 +1,5 @@
 import os
+from tqdm import tqdm
 import time
 import pickle
 from typing import List
@@ -6,15 +7,13 @@ from typing import List
 from transformers import BertModel, BertTokenizer
 from iml_group_proj.features.common.data import load
 
-OUTPUT_ROOT_PATH = "_output/bert_embeddings"
+OUTPUT_ROOT_PATH = "_output/bert_embeddings_2"
 OUTPUT_PATH = "bert_{}.pk"
 BERT_MODEL_NAME = "prajjwal1/bert-tiny"
 MAX_TOKENS_LENGTH = 512 # Can effect the sypnosis, check.
 VERBOSE = True
 
 classes, train, test = load()
-# train = train[:5]
-# test = test[:5]
 bert_tokenizer = BertTokenizer.from_pretrained(BERT_MODEL_NAME)
 model = BertModel.from_pretrained(BERT_MODEL_NAME)
 
@@ -24,7 +23,7 @@ def vprint(s):
 
 def from_sentences_to_bert_embeddings(sentences: List[str]):
     outputs = []
-    for sentence in sentences:
+    for sentence in tqdm(sentences):
         inputs = bert_tokenizer(sentence, truncation=True, max_length=MAX_TOKENS_LENGTH, return_tensors="pt")
         output = model(**inputs)
         outputs.append(output.last_hidden_state.squeeze(0))
@@ -32,19 +31,42 @@ def from_sentences_to_bert_embeddings(sentences: List[str]):
     return outputs
 
 vprint("Getting BERT embeddings for training set.")
-start = time.time()
-train_data = [from_sentences_to_bert_embeddings(sentences) for sentences in [train.title, train.sypnosis]]
-vprint(f"Done! [{time.time() - start:.3f}s], getting BERT embeddings for training set.")
-start = time.time()
-test_data = [from_sentences_to_bert_embeddings(sentences) for sentences in [test.title, test.sypnosis]]
-vprint(f"Done! [{time.time() - start:.3f}s], saving as .pickle files")
-
 os.makedirs(os.path.join(OUTPUT_ROOT_PATH), exist_ok=True)
 
-with open(os.path.join(OUTPUT_ROOT_PATH, OUTPUT_PATH.format("train")), "wb") as f:
-    pickle.dump(train_data, f)
+sypnosis_tasks = [(train.sypnosis[:10000], "train_sypnosis_10k"), (train.sypnosis[10000:20000], "train_sypnosis_20k"), (train.sypnosis[20000:], "train_sypnosis_rest")]
+tasks = [(train.title, "train_title"), (test.title, "test_title"), (test.sypnosis, "test_sypnosis")] + sypnosis_tasks
+for (saving_data, file_name) in tasks:
+    file_path = os.path.join(OUTPUT_ROOT_PATH, OUTPUT_PATH.format(file_name))
+    print(f"Doing {file_path}")
+    if os.path.isfile(file_path):
+        print("File found, skipping...")
+        continue # Skip to the next data
 
-with open(os.path.join(OUTPUT_ROOT_PATH, OUTPUT_PATH.format("test")), "wb") as f:
-    pickle.dump(test_data, f)
+    data = from_sentences_to_bert_embeddings(saving_data)
+    with open(file_path, "wb") as f:
+        pickle.dump(data, f)
 
-vprint(f"Embeddings stored successfully in {OUTPUT_ROOT_PATH}")
+# Combining the splitted sypnosis because too big for my machine :|
+sypnosis_list = []
+for (_, sypnosis_file) in sypnosis_tasks:
+    with open(os.path.join(OUTPUT_ROOT_PATH, OUTPUT_PATH.format(sypnosis_file)), "rb") as f:
+        sypnosis_list.extend(pickle.load(f))
+
+    print(len(sypnosis_list))
+    with open(os.path.join(OUTPUT_ROOT_PATH, OUTPUT_PATH.format("train_sypnosis")), "wb") as f:
+        pickle.dump(sypnosis_list, f)
+
+for title, sypnosis in [("train_title", "train_sypnosis"), ("test_title", "test_sypnosis")]:
+    with open(os.path.join(OUTPUT_ROOT_PATH, OUTPUT_PATH.format(title)), "rb") as f:
+        title_data = pickle.load(f)
+    with open(os.path.join(OUTPUT_ROOT_PATH, OUTPUT_PATH.format(sypnosis)), "rb") as f:
+        sypnosis_data = pickle.load(f)
+
+    assert(len(title_data) == len(sypnosis_data)), f"Title lengths({len(title_data)}) is not equal to sypnosis's({len(sypnosis_data)})"
+
+    with open(os.path.join(OUTPUT_ROOT_PATH, OUTPUT_PATH.format(title.split("_")[0])), "wb") as f:
+        pickle.dump([title_data, sypnosis_data], f)
+    print(f'DONE!, saved at {os.path.join(OUTPUT_ROOT_PATH, OUTPUT_PATH.format(title.split("_")[0]))}')
+
+    # os.remove(os.path.join(OUTPUT_ROOT_PATH, OUTPUT_PATH.format(title))
+    # os.remove(os.path.join(OUTPUT_ROOT_PATH, OUTPUT_PATH.format(sypnosis))
