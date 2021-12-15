46c0199773f9b57ae0bb95873eb35df3b698e27e
Teerapat12 <teerapat.time12@gmail.com>
2021-12-15 06:09:59 -0500

    make bert builder able to continue frome the last download, also make building embeddings more optimal

--- iml_group_proj/data/embeddings.py
+++ iml_group_proj/data/embeddings.py
@@ -4,7 +4,7 @@ import pickle
 import scipy
 import numpy as np
 from abc import ABC, abstractmethod
-from typing import NamedTuple, List, Union
+from typing import NamedTuple, List, Union, Tuple
 
 from iml_group_proj.data.utils import load_libofc_df
 
@@ -29,22 +29,73 @@ class EmbeddingsType:
     TFIDF = "tfidf"
 
 class EmbeddingsDataLoader:
-
+    # TODO: Write specific loader for BERT
     @staticmethod
     def load(file_path: os.PathLike, e_type: str):
-        with open(os.path.join(file_path, f"{e_type}_train.pk"), 'rb') as f:
-            [tr_title_embeddings, tr_synopsis_embeddings] = pickle.load(f)
-
-        with open(os.path.join(file_path, f"{e_type}_test.pk"), 'rb') as f:
-            [te_title_embeddings, te_synopsis_embeddings] = pickle.load(f)
-
         _, train_df, test_df = load_libofc_df(os.path.join(file_path, "trainTest.pk"))
 
+        if "bert" in e_type:
+            data = _load_bert_embeddings(file_path, (train_df.shape[0], test_df.shape[0]), e_type)
+        else:
+            data = _load_other_embeddings(file_path, e_type)
+
+
         return EmbeddingsData(
-                tr_title_embeddings,
-                tr_synopsis_embeddings,
+                data[0],
+                data[1],
                 train_df["class"].values,
-                te_title_embeddings,
-                te_synopsis_embeddings,
+                data[2],
+                data[3],
                 test_df["class"].values
                 )
+
+def _load_other_embeddings(file_path, e_type):
+    with open(os.path.join(file_path, f"{e_type}_train.pk"), 'rb') as f:
+        [tr_title_embeddings, tr_synopsis_embeddings] = pickle.load(f)
+
+    with open(os.path.join(file_path, f"{e_type}_test.pk"), 'rb') as f:
+        [te_title_embeddings, te_synopsis_embeddings] = pickle.load(f)
+
+
+    return (
+            tr_title_embeddings,
+            tr_synopsis_embeddings,
+            te_title_embeddings,
+            te_synopsis_embeddings,
+            )
+
+
+def _load_bert_embeddings(file_path: os.PathLike, expected_length: Tuple[int,int], e_type):
+    old_file_name_format = os.path.join(file_path, f"{e_type}_train.pk")
+    if os.path.isfile(old_file_name_format):
+        return _load_other_embeddings(file_path, e_type)
+
+    folder_path = os.path.join(file_path, e_type)
+
+    tr_title_embeddings = _load_each_bert_embs(folder_path, "title_train", expected_length[0])
+    tr_synop_embeddings = _load_each_bert_embs(folder_path, "synopsis_train", expected_length[0])
+    te_title_embeddings = _load_each_bert_embs(folder_path, "title_test", expected_length[1])
+    te_synop_embeddings = _load_each_bert_embs(folder_path, "synopsis_test", expected_length[1])
+
+    return (tr_title_embeddings, tr_synop_embeddings, te_title_embeddings, te_synop_embeddings)
+
+
+def _load_each_bert_embs(file_path, bert_e_type, expected_length):
+    file_path = os.path.join(file_path, bert_e_type)
+    all_embeddings = [f_name for f_name in os.listdir(file_path) if "pk" in f_name]
+    all_embeddings.sort()
+    assert len(all_embeddings) == expected_length, \
+    f"{bert_e_type} not fully downloaded yet ({len(all_embeddings)}/{expected_length}). \n Please use runner/build_bert_embeddings.py to download the data first."
+
+    embs = []
+    # Do the loading 
+    for embeddings_file in all_embeddings[:5]:
+        emb_file_name = os.path.join(file_path, embeddings_file)
+        print(emb_file_name)
+        with open(emb_file_name, "rb") as f:
+            embs.append(pickle.load(f))
+    return embs
+    
+
+
+
--- runner/build_bert_embeddings.py
+++ runner/build_bert_embeddings.py
@@ -1,6 +1,7 @@
 import os
 import time
 import click
+import shutil
 import pickle
 import pandas as pd
 from tqdm import tqdm
@@ -15,6 +16,18 @@ BERT_NAME_MAPPER = {
         "large":  "bert-base-uncased",
         }
 
+BERT_SIZENUM_MAPPER = {
+        "tiny": 1.0,
+        "small":4.1,
+        "large":10,
+        }
+
+BERT_SIZE_MAPPER = {
+        "tiny": "1.00GB",
+        "small":  "3.99GiB",
+        "large":  "5.99GB",
+        }
+
 OUTPUT_PATH = "bert_{}.pk"
 MAX_TOKENS_LENGTH = 512 # Can effect the synopsis, check.
 VERBOSE = True
@@ -27,45 +40,67 @@ with open(os.path.join(DATA_PATH, "trainTest.pk"), 'rb') as f:
     train = tuple_to_df(pickle.load(f))
     test = tuple_to_df(pickle.load(f))
 
+# Code from: https://www.codegrepper.com/code-examples/python/convert+bytes+to+human+readable+python
+def human_readable_size(size, decimal_places=3):
+    for unit in ['B','KiB','MiB','GiB','TiB']:
+        if size < 1024.0:
+            break
+        size /= 1024.0
+    return f"{size:.{decimal_places}f}{unit}"
+
 @click.command()
 @click.option("--model_size", default="small", help="size of BERT model. expected [tiny, small, large]")
 def build_bert_embeddings(model_size: str):
+
     assert model_size in ["tiny", "small", "large"], f"{model_size} not expected, expected [tiny, small, large]"
 
-    OUTPUT_ROOT_PATH = "github_data/bert_" + model_size + "_{}.pk"
+    print("Getting BERT embeddings...")
+    OUTPUT_ROOT_PATH = os.path.join("github_data", f"bert_{model_size}")
+    os.makedirs(OUTPUT_ROOT_PATH, exist_ok=True)
+
+    disk_stats = shutil.disk_usage(OUTPUT_ROOT_PATH)
+    downloaded_size = sum(os.path.getsize(f) for f in os.listdir(OUTPUT_ROOT_PATH) if os.path.isfile(f))
+    print(f"{BERT_NAME_MAPPER[model_size]} requires a total of {BERT_SIZE_MAPPER[model_size]}: ")
+    if downloaded_size > 0:
+        print(f"Downloaded {human_readable_size(downloaded_size)}")
+
+    if disk_stats.free / (1024 * 1024 * 1024) < BERT_SIZENUM_MAPPER[model_size]:
+        print(f"You have only {human_readable_size(disk_stats.free)} disk space left.")
+        ans = input("Space won't be enough. Are you sure you want to continue loading the embeddings? [y/N]")
+        if ans.lower() != "y":
+            print("Exiting the program")
+            return
     
-    print(f"Downloading ({BERT_NAME_MAPPER[model_size]})")
+    print(f"Downloading ({BERT_NAME_MAPPER[model_size]}) to {OUTPUT_ROOT_PATH}")
     bert_tokenizer = BertTokenizer.from_pretrained(BERT_NAME_MAPPER[model_size])
     model = BertModel.from_pretrained(BERT_NAME_MAPPER[model_size])
 
-    def vprint(s):
-        if VERBOSE:
-            print(s)
+    def get_file_path(file_name, sentence_id):
+        return os.path.join(file_name, f"{sentence_id:06d}.pk")
 
-    def get_path(file_name):
-        return os.path.join(OUTPUT_ROOT_PATH, OUTPUT_PATH.format(file_name))
+    def from_sentences_to_bert_embeddings(sentences: List[str], root_output_path: str):
+        for idx, sentence in tqdm(enumerate(sentences), total=len(sentences)):
+            outputting_file_path = get_file_path(root_output_path, idx)
+            if os.path.isfile(outputting_file_path):
+                continue
 
-    def from_sentences_to_bert_embeddings(sentences: List[str]):
-        outputs = []
-        for sentence in tqdm(sentences):
             inputs = bert_tokenizer(sentence, truncation=True, max_length=MAX_TOKENS_LENGTH, return_tensors="pt")
-            output = model(**inputs)
-            outputs.append(output.last_hidden_state.squeeze(0).detach().numpy())
+            output = model(**inputs).last_hidden_state.squeeze(0).detach().numpy()
 
-        return outputs
+            with open(outputting_file_path, "wb") as f:
+                pickle.dump(output, f)
 
-    vprint("Getting BERT embeddings...")
-    os.makedirs(OUTPUT_ROOT_PATH, exist_ok=True)
+        return True
+    for (titles, synopsis, name) in [(train.title, train.synopsis, "train"), (test.title, test.synopsis, "test")]:
+        title_folder_name = os.path.join(OUTPUT_ROOT_PATH, f"title_{name}")
+        synopsis_folder_name = os.path.join(OUTPUT_ROOT_PATH, f"synopsis_{name}")
+        os.makedirs(title_folder_name, exist_ok=True)
+        os.makedirs(synopsis_folder_name, exist_ok=True)
 
-    for (titles, synopsis, name) in [(train.title, train.synopsis, "train")]:
-        title_embeddings = from_sentences_to_bert_embeddings(titles)
-        synopsis_embeddings = from_sentences_to_bert_embeddings(synopsis)
-        assert len(title_embeddings) == len(synopsis_embeddings), f"Title lengths({len(title_embeddings)}) is not equal to synopsis's({len(synopsis_embeddings)})"
-
-        with open(os.path.join(OUTPUT_ROOT_PATH, OUTPUT_PATH.format(name)), "wb") as f:
-            pickle.dump([title_embeddings, synopsis_embeddings], f)
-
-        vprint(f'DONE!, saved at {os.path.join(OUTPUT_ROOT_PATH, OUTPUT_PATH.format(name))}')
+        from_sentences_to_bert_embeddings(titles, title_folder_name)
+        print(f'DONE!, embeddings saved at {title_folder_name}')
+        from_sentences_to_bert_embeddings(synopsis, synopsis_folder_name)
+        print(f'DONE!, embeddings saved at {synopsis_folder_name}')
 
 if __name__ == "__main__":
     build_bert_embeddings()
